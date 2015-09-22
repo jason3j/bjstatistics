7e7c07530455e04df1c27201723fae5881c09919
Admin <JMac@Chases-MacBook-Pro.local>
2015-09-22 16:57:20 -0600

    files now readable

--- files_to_readable.js
+++ files_to_readable.js
@@ -1,15 +1,25 @@
 var fs = require("fs");
 
-
 // read file in
-// remove punctuation(only leave alphabet characters and single spaces),
+// remove punctuation(only leave alphabet characters and single spaces and 't),
 // lowercase everything, write to new text file, get rid of /r/n
 
-fs.readFile("got_text_files/game_of_thrones", function (err, data) {
+fs.readFile("got_text_files/storm_of_swords.txt", function (err, data) {
     if(err) {
         return console.log(err);
     }
+    var initialBookText = data.toString();
 
-    console.log(data.toString());
+    var cleanText = initialBookText.toLowerCase().replace(/[^A-z’]/g, " ").replace(/’s/g, " ").replace(/\s\s/g, " ").replace(/\s\s\s/g, " ");
 
+    console.log(cleanText);
+
+    fs.writeFile("got_text_files/storm_of_swords_clean.txt", cleanText, function (err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
 });
+
+
